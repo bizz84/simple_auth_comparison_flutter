bce3b7c7f5293b6d29c97559f28bfdb512ad9810
bizz84 <bizz84@gmail.com>
2019-07-01 16:05:13 +0100

    Use authService in LandingPage

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_auth_comparison_flutter/home_page.dart';
+import 'package:simple_auth_comparison_flutter/services/auth_service.dart';
 import 'package:simple_auth_comparison_flutter/sign_in/option.dart';
 import 'package:simple_auth_comparison_flutter/sign_in/sign_in_page_navigation.dart';
 
@@ -9,14 +10,15 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Used to keep track of navigation tab across sign-in events
+    final authService = Provider.of<AuthService>(context);
     return ChangeNotifierProvider<ValueNotifier<Option>>(
       builder: (_) => ValueNotifier<Option>(Option.vanilla),
       // onAuthStateChanged
-      child: StreamBuilder<FirebaseUser>(
-        stream: FirebaseAuth.instance.onAuthStateChanged,
+      child: StreamBuilder<User>(
+        stream: authService.onAuthStateChanged,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.active) {
-            FirebaseUser user = snapshot.data;
+            User user = snapshot.data;
             if (user == null) {
               return Consumer<ValueNotifier<Option>>(
                   builder: (_, ValueNotifier<Option> option, __) => SignInPageNavigation(option: option),
